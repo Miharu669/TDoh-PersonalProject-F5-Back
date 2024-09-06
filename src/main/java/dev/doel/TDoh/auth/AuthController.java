afa0242b887d5563b94f96f8af33aac39120dccf
Vero Doel <veronicadoelfuentes@gmail.com>
2024-09-07 00:53:42 +0200

    Authcontroller done and edit userrepository with findbyemail

---
+++ src/main/java/dev/doel/TDoh/auth/AuthController.java
@@ -0,0 +1,52 @@
+package dev.doel.TDoh.auth;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.doel.TDoh.users.User;
+import dev.doel.TDoh.users.UserRepository;
+
+@RestController
+@RequestMapping(path = "${api-endpoint}")
+public class AuthController {
+
+    private final UserRepository userRepository;
+
+    public AuthController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping(path = "/login")
+    public ResponseEntity<Map<String, String>> login() {
+
+        SecurityContext contextHolder = SecurityContextHolder.getContext();
+        Authentication auth = contextHolder.getAuthentication();
+
+        if (auth == null || !auth.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", "Not authenticated"));
+        }
+
+        String username = auth.getName();
+
+        User user = userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found " + username));
+
+        Map<String, String> json = new HashMap<String, String>();
+        json.put("message", "Logged");
+        json.put("username", auth.getName());
+        json.put("email", user.getEmail());
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(json);
+    }
+}
--- src/main/java/dev/doel/TDoh/users/UserRepository.java
+++ src/main/java/dev/doel/TDoh/users/UserRepository.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long>{
-    
+public interface UserRepository extends JpaRepository<User, Long> {
+
     public Optional<User> findByUsername(String username);
+
+    public Optional<User> findByEmail(String email);
 
 }
