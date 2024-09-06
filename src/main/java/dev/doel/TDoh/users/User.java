ceb9f38ae2c56009c17580e6a62d42a2d9c18b3c
Vero Doel <veronicadoelfuentes@gmail.com>
2024-09-06 23:11:45 +0200

    User and UserRepository added

---
+++ src/main/java/dev/doel/TDoh/users/User.java
@@ -0,0 +1,23 @@
+package dev.doel.TDoh.users;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    private String name;
+    private String email;
+    private String password;
+}
