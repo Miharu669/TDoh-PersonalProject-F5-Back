f30dc85a29693d8ab3183fc783ef66edcf8d512b
Vero Doel <veronicadoelfuentes@gmail.com>
2024-09-06 22:35:16 +0200

    first commit

---
+++ src/main/java/dev/doel/TDoh/TDohApplication.java
@@ -0,0 +1,13 @@
+package dev.doel.TDoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TDohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TDohApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/doel/TDoh/TDohApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.doel.TDoh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TDohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
