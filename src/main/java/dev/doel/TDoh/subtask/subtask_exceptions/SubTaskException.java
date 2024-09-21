6b6a415054a9469d54abbf15d09a08465d80a61e
Vero Doel <veronicadoelfuentes@gmail.com>
2024-09-21 18:56:19 +0200

    added subtask exceptions

---
+++ src/main/java/dev/doel/TDoh/subtask/subtask_exceptions/SubTaskException.java
@@ -0,0 +1,8 @@
+package dev.doel.TDoh.subtask.subtask_exceptions;
+
+public class SubTaskException extends RuntimeException {
+    public SubTaskException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/dev/doel/TDoh/subtask/subtask_exceptions/SubTaskNotFoundException.java
@@ -0,0 +1,8 @@
+package dev.doel.TDoh.subtask.subtask_exceptions;
+
+public class SubTaskNotFoundException extends SubTaskException {
+    public SubTaskNotFoundException(String message) {
+        super(message);
+    }
+    
+}
