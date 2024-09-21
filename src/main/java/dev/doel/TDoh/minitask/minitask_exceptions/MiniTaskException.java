1d6254a777f7def34603609be35f6479fcd64a1e
Vero Doel <veronicadoelfuentes@gmail.com>
2024-09-21 20:27:24 +0200

    exceptions done

---
+++ src/main/java/dev/doel/TDoh/minitask/minitask_exceptions/MiniTaskException.java
@@ -0,0 +1,8 @@
+package dev.doel.TDoh.minitask.minitask_exceptions;
+
+public class MiniTaskException extends RuntimeException {
+    public MiniTaskException(String message) {
+        super(message);
+    }
+    
+}
---
+++ src/main/java/dev/doel/TDoh/minitask/minitask_exceptions/MiniTaskNotFoundException.java
@@ -0,0 +1,8 @@
+package dev.doel.TDoh.minitask.minitask_exceptions;
+
+public class MiniTaskNotFoundException extends MiniTaskException {
+    public MiniTaskNotFoundException(String message) {
+        super(message);
+    }
+    
+}
