3f0f99a205687de05eb87ca6fd91ca4172d9640d
Vero Doel <veronicadoelfuentes@gmail.com>
2024-10-01 21:44:14 +0200

    fixed noteController

--- src/main/java/dev/doel/TDoh/notes/NoteController.java
+++ src/main/java/dev/doel/TDoh/notes/NoteController.java
@@ -3,10 +3,12 @@ package dev.doel.TDoh.notes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
+import dev.doel.TDoh.users.User;
+
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -18,29 +20,32 @@ public class NoteController {
     private final NoteService noteService;
 
     @GetMapping
-    public ResponseEntity<List<NoteDTO>> getUserNotes(@AuthenticationPrincipal UserDetails userDetails) {
-        Long userId = Long.parseLong(userDetails.getUsername()); 
-        List<NoteDTO> notes = noteService.getNotesByUserId(userId);
+    public ResponseEntity<List<NoteDTO>> getUserNotes(Principal connectedUser) {
+        User user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+        List<NoteDTO> notes = noteService.getNotesByUserId(user.getId());
         return ResponseEntity.ok(notes);
     }
 
     @PostMapping
-    public ResponseEntity<NoteDTO> createNote(@AuthenticationPrincipal UserDetails userDetails, @RequestBody NoteDTO noteDTO) {
-        Long userId = Long.parseLong(userDetails.getUsername());
-        NoteDTO createdNote = noteService.createNote(userId, noteDTO);
+    public ResponseEntity<NoteDTO> createNote(Principal connectedUser,@RequestBody NoteDTO noteDTO) {
+        User user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+        NoteDTO createdNote = noteService.createNote(user.getId(), noteDTO);
         return ResponseEntity.ok(createdNote);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<NoteDTO> updateNote(@PathVariable Long id, @RequestBody NoteDTO noteDTO) {
-        NoteDTO updatedNote = noteService.updateNoteById(id, noteDTO);
+    public ResponseEntity<NoteDTO> updateNote(Principal connectedUser, @RequestBody NoteDTO noteDTO) {
+        User user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+        NoteDTO updatedNote = noteService.updateNoteById(user.getId(), noteDTO);
 
         return ResponseEntity.ok(updatedNote);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteNoteById(@PathVariable Long id) {
-        noteService.deleteNoteById(id);
+    public ResponseEntity<Void> deleteNoteById(Principal connectedUser) {
+        User user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+
+        noteService.deleteNoteById(user.getId());
         return ResponseEntity.noContent().build();
     }
 
